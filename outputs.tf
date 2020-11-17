a36491c234d73d252d60fb16d64f2bb92b2ebac0
bryam hererra <bryam.hf@gmail.com>
2020-11-17 14:51:22 -0500

    se agrego salida al modulo para el DNS_NAME

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "elb_id" {
   value       = aws_alb.elb.id
   description = "The name vpc"
 }
+output "elb_dns" {
+  value       = aws_alb.elb.dns_name
+  description = "The name vpc"
+}
 output "targetGroup_id" {
   value       = aws_alb_target_group.TargetGroup.id
   description = "The name vpc"
