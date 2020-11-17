95e0b6b3cad243ae0e6d9a767630f1d39a5d957e
bryam hererra <bryam.hf@gmail.com>
2020-11-17 12:21:13 -0500

    se agrega nueva salida del modulo elb.id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "elb_id" {
+  value       = aws_alb.elb.id
+  description = "The name vpc"
+}
 output "targetGroup_id" {
   value       = aws_alb_target_group.TargetGroup.id
   description = "The name vpc"
