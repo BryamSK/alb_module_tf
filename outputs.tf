73368aa25087f40a58b82884f2a341d7cedfeb4a
bryam hererra <bryam.hf@gmail.com>
2020-11-17 13:45:01 -0500

    se configuran las salidas del modulo

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "targetGroup_id" {
   value       = aws_alb_target_group.TargetGroup.id
   description = "The name vpc"
 }
+output "alb_frontend_id" {
+  value       = aws_alb_listener.front_end.id
+  description = "The name vpc"
+}
